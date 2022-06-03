f5a7240874f076833b562b0f3de5729cc8344b75
Vasilenko.man <106017162+VasilenkoAlexander@users.noreply.github.com>
2022-06-03 11:46:30 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 1,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });
